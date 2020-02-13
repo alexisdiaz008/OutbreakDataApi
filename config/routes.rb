ec8e198ca7e6092cb91d2e48ce32b55115266836
alexisdiaz008 <lexmasta@gmail.com>
2020-02-12 21:15:44 -0500

    outbreak controller versioning and route version namespaces added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :out_breaks
+	namespace :api do
+		namespace :v1 do
+			resources :out_breaks
+		end
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
